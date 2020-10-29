e9f6043212c91223d1c01887c7814754f13db087
Maciej Błażewicz <224264@edu.p.lodz.pl>
2020-10-29 21:39:22 +0100

    Added Client entity

---
+++ Task_1/Store/Entities/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Store.Entities
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} | Surname {Surname} | Email {Email} | Address: {Address}";
+        }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return $"Street: {Street} | City: {City}";
+        }
+    }
+}
