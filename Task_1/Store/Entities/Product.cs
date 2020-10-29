d677408b6289de765ae43ec5f80d9a42a4a0ae35
Nawrok <nawrok999@gmail.com>
2020-10-29 22:25:19 +0100

    Added Product entity

---
+++ Task_1/Store/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Store.Entities
+{
+    class Product
+    {
+        public Product() { }
+
+        public Product(Guid id, string name, string description, string type)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Type = type;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} | Name: {Name} | Description: {Description} | Type: {Type}";
+        }
+    }
+}
