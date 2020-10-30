541f771ff9b1e0b11a63616ea33d84d28e6811ad
Nawrok <nawrok999@gmail.com>
2020-10-30 12:16:01 +0100

    Added DataContext with initialized properties

---
+++ Task_1/Store/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Store.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Store
+{
+    public class DataContext
+    {
+        public List<Client> Clients { get; } = new List<Client>();
+        public Dictionary<Guid, Product> Products { get; } = new Dictionary<Guid, Product>();
+        public List<Offer> Offers { get; } = new List<Offer>();
+        public ObservableCollection<Facture> Factures = new ObservableCollection<Facture>();
+    }
+}
