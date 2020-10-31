324b7510dd3e130aafe1f85ed1c189c95bd523fa
Maciej Błażewicz <224264@edu.p.lodz.pl>
2020-11-01 00:01:56 +0100

    Added interface for DataRepository with CRUD methods for every collections from DataContext

---
+++ Task_1/Store/IDataRepository.cs
@@ -0,0 +1,31 @@
+﻿using Store.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    public interface IDataRepository
+    {
+        void AddClient(Client client);
+        void AddProduct(Product product);
+        void AddOffer(Offer offer);
+        void AddFacture(Facture facture);
+        Client GetClient(string email);
+        Product GetProduct(Guid productId);
+        Offer GetOffer(Guid offerId);
+        Facture GetFacture(Guid factureId);
+        IEnumerable<Client> GetClients();
+        IEnumerable<Product> GetProducts();
+        IEnumerable<Offer> GetOffers();
+        IEnumerable<Facture> GetFactures();
+        void UpdateClient(string email, Client client);
+        void UpdateProduct(Guid productId, Product product);
+        void UpdateOffer(Guid offerId, Offer offer);
+        void UpdateFacture(Guid factureId, Facture facture);
+        void DeleteClient(Client client);
+        void DeleteProduct(Product product);
+        void DeleteOffer(Offer offer);
+        void DeleteFacture(Facture facture);
+    }
+}
