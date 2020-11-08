a02eca21e5c9dcd0c051ea702dfe6254f0c46e07
Maciej Błażewicz <224264@edu.p.lodz.pl>
2020-11-08 16:49:09 +0100

    Tweaked environment for logic tests

--- Task_1/Store.Tests/DataServiceTest.cs
+++ Task_1/Store.Tests/DataServiceTest.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Store.BLL;
 using Store.DAL;
+using Store.DAL.Model;
 using Store.Tests.Filler;
+using System;
 
 namespace Store.Tests
 {
@@ -9,53 +11,103 @@ namespace Store.Tests
     public class DataServiceTest
     {
         private DataService _dataService;
+        private Client _c1;
+        private Product _p1;
+        private Offer _o1;
+        private Event _e1;
 
         [TestInitialize]
         public void TestInitialize()
         {
             _dataService = new DataService(new DataRepository(new ConstDataFiller()));
+            _c1 = new Client("Norbert", "Gierczak", "krzycz@disunio.pl", "Katowice");
+            _p1 = new Product(Guid.NewGuid(), "Topór", "Broń ostra, można nią rzucać", "Broń");
+            _o1 = new Offer(_p1, 450.00m, 0.23m, 2);
+            _e1 = new Facture(Guid.NewGuid(), _c1, _o1, 1, DateTimeOffset.Now.AddDays(-2));
         }
 
         [TestMethod]
-        public void AddClient_Test() { }
+        public void AddClient_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void AddProduct_Test() { }
+        public void AddProduct_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void AddOffer_Test() { }
+        public void AddOffer_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void DeleteProduct_Test() { }
+        public void DeleteProduct_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void DeleteOffer_Test() { }
+        public void DeleteOffer_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void BuyProducts_Test() { }
+        public void BuyProducts_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void ReturnProducts_Test() { }
+        public void ReturnProducts_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void UpdateOfferState_Test() { }
+        public void UpdateOfferState_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void GetFacturesForClient_Test() { }
+        public void GetFacturesForClient_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void GetReturnsForClient_Test() { }
+        public void GetReturnsForClient_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void GetFacturesForProduct_Test() { }
+        public void GetFacturesForProduct_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void GetClientsForProduct_Test() { }
+        public void GetClientsForProduct_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void GetBoughtProductsForClient_Test() { }
+        public void GetBoughtProductsForClient_Test()
+        {
+            Assert.Inconclusive();
+        }
 
         [TestMethod]
-        public void GetProductSales_Test() { }
+        public void GetProductSales_Test()
+        {
+            Assert.Inconclusive();
+        }
     }
 }
