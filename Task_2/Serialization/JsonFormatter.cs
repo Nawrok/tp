b95f3fa6125b526f1833f59cdab6e4ca7e83a8fb
Maciej Błażewicz <224264@edu.p.lodz.pl>
2020-11-17 23:53:16 +0100

    Implemented JsonFormatter class

--- Task_2/Serialization/JsonFormatter.cs
+++ Task_2/Serialization/JsonFormatter.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.IO;
 using System.Text;
 
 namespace Serialization
 {
-    class JsonFormatter
+    public class JsonFormatter
     {
+        public static void Serialize(object obj, Stream stream)
+        {
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+            });
+
+            byte[] serializedContent = Encoding.UTF8.GetBytes(json);
+            stream.Write(serializedContent, 0, serializedContent.Length);
+        }
+
+        public static T Deserialize<T>(Stream stream)
+        {
+            StreamReader reader = new StreamReader(stream);
+
+            string fileContent = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<T>(fileContent, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+            });
+        }
     }
 }
