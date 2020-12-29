d746602336b3c6bab792d7a360af9bf1845045b0
Maciej Błażewicz <224264@edu.p.lodz.pl>
2020-12-29 16:02:13 +0100

    Created place for third subtask

---
+++ Task_3/LINQ.Tests/ToolsTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LINQ;
+
+namespace LINQ.Tests
+{
+    [TestClass]
+    public class ToolsTest
+    {
+        [TestMethod]
+        public void GetProductsByNameTest()
+        {
+            List<Product> products = Tools.GetProductsByName("Minipump");
+            Assert.AreEqual(products.Count(), 1);
+            Assert.AreEqual(products[0].ProductID, 844);
+            Assert.AreEqual(products[0].ProductNumber, "PU-0452");
+        }
+    }
+}
---
+++ Task_3/LINQ/Tools.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public static class Tools
+    {
+        public static List<Product> GetProductsByName(string namePart)
+        {
+            using (ProductionDataContext _dataContext = new ProductionDataContext())
+            {
+                Table<Product> products = _dataContext.GetTable<Product>();
+                List<Product> result = (from product in products
+                                        where product.Name.Contains(namePart)
+                                        select product).ToList();
+                return result;
+            }
+        }
+    }
+}
