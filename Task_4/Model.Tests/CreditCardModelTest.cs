77c77e90ec391313b3fcf80c026ff6619586a433
Maciej Błażewicz <224264@edu.p.lodz.pl>
2021-01-17 15:59:35 +0100

    Added Model tests

---
+++ Task_4/Model.Tests/CreditCardModelTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Model.Tests
+{
+    [TestClass]
+    public class CreditCardModelTest
+    {
+        private CreditCardModel _card1;
+        private CreditCardModel _card2;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _card2 = new CreditCardModel
+            {
+                CardNumber = "11122233344455",
+                CardType = "Vista",
+                ExpMonth = 6,
+                ExpYear = 21
+            };
+        }
+
+        [TestMethod]
+        public void CreditCardModelNull()
+        {
+            Assert.AreEqual(null, _card1);
+        }
+
+        [TestMethod]
+        public void CreditCardModelCorrect()
+        {
+            Assert.IsNotNull(_card2);
+            Assert.AreEqual("11122233344455", _card2.CardNumber);
+            Assert.AreEqual("Vista", _card2.CardType);
+            Assert.AreEqual(6, _card2.ExpMonth);
+            Assert.AreEqual(21, _card2.ExpYear);
+        }
+    }
+}
