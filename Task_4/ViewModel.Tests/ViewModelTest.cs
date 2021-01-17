ebdfa3da5b2a742fba9a73814254384a640f807c
Maciej Błażewicz <224264@edu.p.lodz.pl>
2021-01-17 23:38:26 +0100

    Added tests for ViewModel

---
+++ Task_4/ViewModel.Tests/ViewModelTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ViewModel;
+
+namespace ViewModel.Tests
+{
+    [TestClass]
+    public class ViewModelTest
+    {
+        [TestMethod]
+        public void CreditCardViewModelCtorTest()
+        {
+            CreditCardViewModel creditCardViewModel = new CreditCardViewModel();
+            Assert.IsNotNull(creditCardViewModel.CardNumber);
+            Assert.IsNotNull(creditCardViewModel.CardType);
+            Assert.IsNotNull(creditCardViewModel.ExpMonth);
+            Assert.IsNotNull(creditCardViewModel.ExpYear);
+        }
+
+        [TestMethod]
+        public void CreditCardListViewModelCtorTest()
+        {
+            CreditCardListViewModel creditCardListViewModel = new CreditCardListViewModel();
+            Assert.IsNotNull(creditCardListViewModel.CreditCardList);
+        }
+
+        [TestMethod]
+        public void CreditCardViewModelCommandsTest()
+        {
+            CreditCardViewModel creditCardViewModel = new CreditCardViewModel();
+            Assert.IsTrue(creditCardViewModel.UpdateCommand.CanExecute(null));
+            Assert.IsTrue(creditCardViewModel.CancelCommand.CanExecute(null));
+            Assert.IsTrue(creditCardViewModel.DeleteCommand.CanExecute(null));
+            Assert.IsTrue(creditCardViewModel.ShowEditCommand.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void CreditCardListViewModelCommandsTest()
+        {
+            CreditCardListViewModel creditCardListViewModel = new CreditCardListViewModel();
+            Assert.IsTrue(creditCardListViewModel.ShowEditCommand.CanExecute(null));
+            Assert.IsTrue(creditCardListViewModel.ShowAddCommand.CanExecute(null));
+        }
+    }
+}
