e6263b040f6555c9d2ef57c9facf55cf25416d8d
Nawrok <nawrok999@gmail.com>
2021-01-19 23:01:39 +0100

    Changed ViewModelTest to use InMemoryService

--- Task_4/ViewModel.Tests/ViewModelTest.cs
+++ Task_4/ViewModel.Tests/ViewModelTest.cs
@@ -1,43 +1,59 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Logic.Tests.Instrumentation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
 
 namespace ViewModel.Tests
 {
     [TestClass]
     public class ViewModelTest
     {
+        private CreditCardViewModel _creditCardViewModel;
+        private CreditCardListViewModel _creditCardListViewModel;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            CreditCardService service = new CreditCardService(new InMemoryCreditCardService());
+            CreditCardModel cardModel = new CreditCardModel
+            {
+                CardNumber = "12345678902137",
+                CardType = "MasterCard",
+                ExpMonth = 12,
+                ExpYear = 23
+            };
+            _creditCardViewModel = new CreditCardViewModel(cardModel, service);
+            _creditCardListViewModel = new CreditCardListViewModel(service);
+        }
+
         [TestMethod]
         public void CreditCardViewModelCtorTest()
         {
-            CreditCardViewModel creditCardViewModel = new CreditCardViewModel();
-            Assert.IsNotNull(creditCardViewModel.CardNumber);
-            Assert.IsNotNull(creditCardViewModel.CardType);
-            Assert.IsNotNull(creditCardViewModel.ExpMonth);
-            Assert.IsNotNull(creditCardViewModel.ExpYear);
+            Assert.IsNotNull(_creditCardViewModel.CardNumber);
+            Assert.IsNotNull(_creditCardViewModel.CardType);
+            Assert.IsNotNull(_creditCardViewModel.ExpMonth);
+            Assert.IsNotNull(_creditCardViewModel.ExpYear);
         }
 
         [TestMethod]
         public void CreditCardListViewModelCtorTest()
         {
-            CreditCardListViewModel creditCardListViewModel = new CreditCardListViewModel();
-            Assert.IsNotNull(creditCardListViewModel.CreditCardList);
+            Assert.IsNotNull(_creditCardListViewModel.CreditCardList);
         }
 
         [TestMethod]
         public void CreditCardViewModelCommandsTest()
         {
-            CreditCardViewModel creditCardViewModel = new CreditCardViewModel();
-            Assert.IsTrue(creditCardViewModel.UpdateCommand.CanExecute(null));
-            Assert.IsTrue(creditCardViewModel.CancelCommand.CanExecute(null));
-            Assert.IsTrue(creditCardViewModel.DeleteCommand.CanExecute(null));
-            Assert.IsTrue(creditCardViewModel.ShowEditCommand.CanExecute(null));
+            Assert.IsTrue(_creditCardViewModel.UpdateCommand.CanExecute(null));
+            Assert.IsTrue(_creditCardViewModel.CancelCommand.CanExecute(null));
+            Assert.IsTrue(_creditCardViewModel.DeleteCommand.CanExecute(null));
+            Assert.IsTrue(_creditCardViewModel.ShowEditCommand.CanExecute(null));
         }
 
         [TestMethod]
         public void CreditCardListViewModelCommandsTest()
         {
-            CreditCardListViewModel creditCardListViewModel = new CreditCardListViewModel();
-            Assert.IsTrue(creditCardListViewModel.ShowEditCommand.CanExecute(null));
-            Assert.IsTrue(creditCardListViewModel.ShowAddCommand.CanExecute(null));
+            Assert.IsTrue(_creditCardListViewModel.ShowEditCommand.CanExecute(null));
+            Assert.IsTrue(_creditCardListViewModel.ShowAddCommand.CanExecute(null));
         }
     }
 }
