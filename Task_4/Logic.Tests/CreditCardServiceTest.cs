b79e383f89bc5e2661e961aa203f2310b3eac14a
Nawrok <nawrok999@gmail.com>
2021-01-19 22:39:29 +0100

    Added tests to Model Improved tests in Logic

---
+++ Task_4/Logic.Tests/CreditCardServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using Logic.Tests.Instrumentation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logic.Tests
+{
+    [TestClass]
+    public class CreditCardServiceTest
+    {
+        private CreditCard _creditCard;
+        private ICreditCardService _creditCardService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _creditCard = new CreditCard
+            {
+                CardNumber = "12345678902137",
+                CardType = "MasterCard",
+                ExpMonth = 12,
+                ExpYear = 23
+            };
+            _creditCardService = new InMemoryCreditCardService();
+        }
+
+        [TestMethod]
+        public void AddCreditCardTest()
+        {
+            Assert.AreEqual(2, _creditCardService.GetAllCreditCards().Count());
+            _creditCardService.AddCreditCard(_creditCard);
+            Assert.AreEqual(3, _creditCardService.GetAllCreditCards().Count());
+        }
+
+        [TestMethod]
+        public void GetCreditCardTest()
+        {
+            _creditCardService.AddCreditCard(_creditCard);
+            Assert.AreEqual(_creditCard.CardNumber, _creditCardService.GetCreditCard(_creditCard.CardNumber).CardNumber);
+        }
+
+        [TestMethod]
+        public void GetAllCreditCardsTest()
+        {
+            Assert.AreEqual(2, _creditCardService.GetAllCreditCards().Count());
+        }
+
+        [TestMethod]
+        public void UpdateCreditCardTest()
+        {
+            Assert.AreEqual(2, _creditCardService.GetAllCreditCards().Count());
+            _creditCardService.UpdateCreditCard(_creditCardService.GetAllCreditCards().Last().CardNumber, _creditCard);
+            Assert.AreEqual(2, _creditCardService.GetAllCreditCards().Count());
+            Assert.AreEqual(_creditCard.CardNumber, _creditCardService.GetAllCreditCards().Last().CardNumber);
+        }
+
+        [TestMethod]
+        public void DeleteCreditCardTest()
+        {
+            Assert.AreEqual(2, _creditCardService.GetAllCreditCards().Count());
+            _creditCardService.AddCreditCard(_creditCard);
+            Assert.AreEqual(3, _creditCardService.GetAllCreditCards().Count());
+            _creditCardService.DeleteCreditCard(_creditCardService.GetAllCreditCards().Last().CardNumber);
+            Assert.AreEqual(2, _creditCardService.GetAllCreditCards().Count());
+        }
+    }
+}
---
+++ Task_4/Model.Tests/CreditCardServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using Logic.Tests.Instrumentation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Model.Tests
+{
+    [TestClass]
+    public class CreditCardServiceTest
+    {
+        private CreditCardService _cardService;
+        private CreditCardModel _creditCard;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _creditCard = new CreditCardModel
+            {
+                CardNumber = "12345678902137",
+                CardType = "MasterCard",
+                ExpMonth = 12,
+                ExpYear = 23
+            };
+            _cardService = new CreditCardService(new InMemoryCreditCardService());
+        }
+
+        [TestMethod]
+        public void AddCreditCardTest()
+        {
+            Assert.AreEqual(2, _cardService.GetAllCreditCards().Count());
+            _cardService.AddCreditCard(_creditCard);
+            Assert.AreEqual(3, _cardService.GetAllCreditCards().Count());
+        }
+
+        [TestMethod]
+        public void GetCreditCardTest()
+        {
+            _cardService.AddCreditCard(_creditCard);
+            Assert.AreEqual(_creditCard.CardNumber, _cardService.GetCreditCard(_creditCard.CardNumber).CardNumber);
+        }
+
+        [TestMethod]
+        public void GetAllCreditCardsTest()
+        {
+            Assert.AreEqual(2, _cardService.GetAllCreditCards().Count());
+        }
+
+        [TestMethod]
+        public void UpdateCreditCardTest()
+        {
+            Assert.AreEqual(2, _cardService.GetAllCreditCards().Count());
+            _cardService.UpdateCreditCard(_cardService.GetAllCreditCards().Last().CardNumber, _creditCard);
+            Assert.AreEqual(2, _cardService.GetAllCreditCards().Count());
+            Assert.AreEqual(_creditCard.CardNumber, _cardService.GetAllCreditCards().Last().CardNumber);
+        }
+
+        [TestMethod]
+        public void DeleteCreditCardTest()
+        {
+            Assert.AreEqual(2, _cardService.GetAllCreditCards().Count());
+            _cardService.AddCreditCard(_creditCard);
+            Assert.AreEqual(3, _cardService.GetAllCreditCards().Count());
+            _cardService.DeleteCreditCard(_cardService.GetAllCreditCards().Last().CardNumber);
+            Assert.AreEqual(2, _cardService.GetAllCreditCards().Count());
+        }
+    }
+}
